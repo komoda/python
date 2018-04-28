89f0bf9e6b34719f3e68d515dff0f1458c8a5b4d
komoda <komoda@hinet.net>
2018-04-28 10:35:56 +0800

    ver 1

--- hello1.py
+++ hello1.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "Hello Wodsfsdfsdrld!"
+   return "Hello Wodsfsdfsfsdfdssdrld!"
 
 if __name__ == '__main__':
    app.run(debug=True)
